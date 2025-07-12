ab671e63a88cf3dee41e2aa090dad942e3ac4d72
Trinity Melder <34265871+TrinityMelder@users.noreply.github.com>
2025-07-13 00:49:54 +1000

    Update sad_1.py

--- sad_1.py
+++ sad_1.py
@@ -5,15 +5,22 @@ import math
 
 NUM_LEDS = 16
 pixels = neopixel.NeoPixel(board.D18, NUM_LEDS, auto_write=False)
-sad_color = (0, 0, 180)  
 
-def sadness_fade(color, steps=50, delay=0.035):
+
+base_color = (0, 0, 255)
+brightness_scale = 0.3  
+sad_color = tuple(int(c * brightness_scale) for c in base_color)
+
+def sadness_fade(color, steps=80, delay=0.05, pause=0.5):
     while True:
-        for i in range(steps):
-            brightness = math.sin(i / steps * math.pi)
+       
+        for i in range(steps, -1, -1):
+            brightness = i / steps  
             scaled_color = tuple(int(c * brightness) for c in color)
             pixels.fill(scaled_color)
             pixels.show()
             time.sleep(delay)
+        
+        time.sleep(pause)
 
 sadness_fade(sad_color)
