086379b6d4276d0a6f1c121f6c6e1cfdc5629c4d
Trinity Melder <34265871+TrinityMelder@users.noreply.github.com>
2025-07-13 00:55:38 +1000

    Update surprise_1.py

--- surprise_1.py
+++ surprise_1.py
@@ -5,26 +5,29 @@ import time
 NUM_LEDS = 16
 pixels = neopixel.NeoPixel(board.D18, NUM_LEDS, auto_write=False)
 
+
+surprise_color = (76, 76, 76)
+
 while True:
-
+    
     for i in range(NUM_LEDS):
-        pixels[i] = (255, 255, 255)
+        pixels[i] = surprise_color
         pixels.show()
-        time.sleep(0.07) 
-
+        time.sleep(0.07)
 
     time.sleep(0.3)
 
-    
+ 
     pixels.fill((0, 0, 0))
     pixels.show()
     time.sleep(0.1)
 
-    pixels.fill((255, 255, 255))
+
+    pixels.fill(surprise_color)
     pixels.show()
     time.sleep(0.15)
 
-    # Rapid decay
+   
     pixels.fill((0, 0, 0))
     pixels.show()
     time.sleep(0.5)
